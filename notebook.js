7da85532ecd524e9926b94ef345e9f1d6987a8ba
Lavish Kumar <129793824+LavishK770@users.noreply.github.com>
2023-07-16 13:11:17 +0530

    A Simple NoteBook Page
    You can download your text by clicking on the 'Design' text

---
+++ notebook.js
@@ -0,0 +1,18 @@
+function download() {
+    var text = document.getElementById("notes").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain" });
+    var anchor = document.createElement("a");
+    anchor.download = "my-filename.txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target = "_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+    emptyText();
+  }
+  function emptyText() {
+    document.getElementById("notes").value = "";
+  }
+  
